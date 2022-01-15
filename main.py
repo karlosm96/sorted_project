5e095df3e3016cf3746e84fba7536980009f5ba4
CarlosM <karlos_m96@hotmail.com>
2022-01-14 21:12:02 -0500

    changes

--- main.py
+++ main.py
@@ -9,8 +9,15 @@ class screen_information:
     RED = 255, 0, 0
     GREEN = 0, 255, 0
     BACKGROUND_COLOR = WHITE
+
+    RECTANGULAR_GRADIENTS = [
+        (128, 128, 128),
+        (160, 160, 160),
+        (192, 192, 192)
+    ]
+
     SIDE_PAD = 100
-    TOP_PAD = 120
+    TOP_PAD = 150
 
     def __init__(self, height, width, lst):
         self.height = height
@@ -24,7 +31,7 @@ class screen_information:
         self.min_value = min(lst)
         self.max_value = max(lst)
 
-        self.pixel_width = round((self.SIDE_PAD - self.width) / len(lst)) ##determines the width of each pixel that will represent the values of the list
+        self.pixel_width = round((self.width - self.SIDE_PAD) / len(lst)) ##determines the width of each pixel that will represent the values of the list
         self.pixel_height = round((self.height - self.TOP_PAD) / (self.max_value - self.min_value))
         self.start_draw_x = self.SIDE_PAD // 2
 
@@ -38,20 +45,40 @@ def generation_list(n, min_val, max_val):
     
     return lst
 
+##change tha screen attributes
+def draw(draw_information):
+    draw_information.window.fill(draw_information.BACKGROUND_COLOR)
+    draw_list(draw_information)
+    pygame.display.update()
+
+    ##--------------------------
+
+def draw_list(draw_information):
+    lst = draw_information.lst
+    
+    for i, val in enumerate(lst):
+        x = draw_information.start_draw_x + i * draw_information.pixel_width  ###
+        y = draw_information.height - (val - draw_information.min_value) * draw_information.pixel_height ###
+
+        color = draw_information.RECTANGULAR_GRADIENTS[i % 3]
+
+        pygame.draw.rect(draw_information.window, color, (x, y, draw_information.pixel_width, draw_information.height))
+
 ##main function where is going to draw the algorithm process
 def main():
     run = True
     clock = pygame.time.Clock()
-    lst = generation_list(50, 1, 50)
-    draw_information = screen_information(600, 400, lst)
+    lst = generation_list(100, 1, 20)
+    draw_information = screen_information(500, 600, lst)
 
     while run:
         clock.tick(60)
+        draw(draw_information)
 
         pygame.display.update()
 
         for event in pygame.event.get():
-            if event == pygame.QUIT:
+            if event.type == pygame.QUIT:
                 run = False
 
     pygame.quit()
